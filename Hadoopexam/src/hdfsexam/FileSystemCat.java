fd1c30f79569b35f659f625a7c1cbfb2c3e1f94f
heejung-choi <choi951008@naver.com>
2020-04-29 16:53:17 +0900

    20200429 리눅스 &하둡 스터디

---
+++ Hadoopexam/src/hdfsexam/FileSystemCat.java
@@ -0,0 +1,23 @@
+package hdfsexam;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+public class FileSystemCat {
+   public static void main(String[] args) throws Exception {
+	Configuration conf = new Configuration();
+	conf.set("fs.defaultFS", "hdfs://192.168.111.120:9000");
+	//<value>hdfs://master:9000/</value>
+	//하둡교육자료 7페이지에 나와있는 내용과 비슷하다.
+	FileSystem hdfs = FileSystem.get(conf);
+	FSDataInputStream in = null;
+	try {
+	    in = hdfs.open(new Path("/edudata/president_moon.txt"));
+	    IOUtils.copyBytes(in, System.out, 4096, false);
+	} finally {
+   	    IOUtils.closeStream(in);
+	}
+    }
+}
